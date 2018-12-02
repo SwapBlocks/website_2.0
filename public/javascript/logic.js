5d470d87a2f7d3e1f529dccaf24e41c5d5f38afb
Ethan <erogers999@gmail.com>
2018-12-01 19:38:53 -0500

    fixed responsive menu

--- public/javascript/logic.js
+++ public/javascript/logic.js
@@ -37,6 +37,85 @@ $("#faqq").click(function() {
     }, 1400);
 });
 
+// MOBILE NAV
+$(document).ready(function () {
+    $("#mobile-nav").hide();
+    $("#mobile-menu-button-2").hide();
+    
+    var openMobile = function() {
+        $("#mobile-menu-button").hide();
+        $("#mobile-menu-button-2").show();
+        $("#mobile-nav").slideDown();
+    }
+    $("#mobile-menu-button").click(function() {
+        openMobile();  
+    });
+    var closeMobile = function() {
+        $("#mobile-menu-button-2").hide();
+        $("#mobile-menu-button").show();
+        $("#mobile-nav").slideUp();
+        
+    }
+    $("#mobile-menu-button-2").click(function() {
+        closeMobile();
+    });
+});
+
+
+$("#home-2").click(function() {
+    $("#mobile-nav").slideUp();
+    $("#mobile-menu-button-2").hide();
+    $("#mobile-menu-button").show();
+    $('html, body').animate({
+        scrollTop: $("#section-1").offset().top
+    }, 1400);
+});
+
+$("#about-2").click(function() {
+    $("#mobile-nav").slideUp();
+    $("#mobile-menu-button-2").hide();
+    $("#mobile-menu-button").show();
+    $('html, body').animate({
+        scrollTop: $("#section-2").offset().top
+    }, 1400);
+});
+
+$("#distribution-2").click(function() {
+    $("#mobile-nav").slideUp();
+    $("#mobile-menu-button-2").hide();
+    $("#mobile-menu-button").show();
+    $('html, body').animate({
+        scrollTop: $("#coin-dis").offset().top
+    }, 1400);
+});
+
+$("#roadmap-2").click(function() {
+    $("#mobile-nav").slideUp();
+    $("#mobile-menu-button-2").hide();
+    $("#mobile-menu-button").show();
+    $('html, body').animate({
+        scrollTop: $("#section-4").offset().top
+    }, 1400);
+});
+
+$("#team-2").click(function() {
+    $("#mobile-nav").slideUp();
+    $("#mobile-menu-button-2").hide();
+    $("#mobile-menu-button").show();
+    $('html, body').animate({
+        scrollTop: $("#section-5").offset().top
+    }, 1400);
+});
+
+$("#faqq-2").click(function() {
+    $("#mobile-nav").slideUp();
+    $("#mobile-menu-button-2").hide();
+    $("#mobile-menu-button").show();
+    $('html, body').animate({
+        scrollTop: $("#section-6").offset().top
+    }, 1400);
+});
+
 
 
 // FAQ
@@ -93,19 +172,71 @@ function drawChart() {
 $(document).ready(function() {
     Particles.init({
         selector: '.particles',
-        maxParticles: 30,
-        
+        sizeVariations: 4,
+        speed: .4,
+        maxParticles: 40,
+        color: ['#222e40', '#8da4be'],
+        connectParticles: true,
+        minDistance: 100,
         responsive: [
             {
-                breakpoint: 1680,
+                breakpoint: 768,
                 options: {
-                    maxParticles: 0,
-                    color: ['#222e40', '#8da4be'],
-                    sizeVariations: 3,
-                    minDistance: 90,
-                    connectParticles: true
+                    maxParticles: 40,
+                    connectParticles: false
                 }
             }
         ]
     });
 });
+
+
+
+
+
+// Scroll Animations
+// var a = 0;
+// $(window).scroll(function() {
+
+//   var oTop = $('#counter').offset().top - window.innerHeight;
+//   if (a == 0 && $(window).scrollTop() > oTop) {
+//     $('.counter-value').each(function() {
+//       var $this = $(this),
+//         countTo = $this.attr('data-count');
+//       $({
+//         countNum: $this.text() 
+//       }).animate({
+//           countNum: countTo
+//         },
+
+//         {
+
+//           duration: 1500,
+//           easing: 'swing',
+//           step: function() {
+//             $this.text(Math.floor(this.countNum));
+//           },
+//           complete: function() {
+//             $this.text(this.countNum);
+//             //alert('finished');
+//           }
+
+//         });
+//     });
+//     a = 1;
+//   }
+
+// });
+
+
+
+$(document).ready( function() {
+    $('.percents').hide();
+})
+
+$(document).scroll(function () {
+    var y = $(this).scrollTop();
+    if (y > 2200) {
+        $('.percents').slideDown().fadeIn();
+    }
+})
